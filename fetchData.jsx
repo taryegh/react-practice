e9767cda671c00dff9e02f6a76dd83577c3fe2dd
Taron Yeghoyan <49352957+taryegh@users.noreply.github.com>
2019-08-02 12:40:22 +0400

    Create fetchData.jsx

---
+++ fetchData.jsx
@@ -0,0 +1,35 @@
+import React, {Component} from "react"
+
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            loading: false,
+            character: {}
+        }
+    }
+    
+    componentDidMount() {
+        this.setState({loading: true})
+        fetch("https://swapi.co/api/people/1")
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    loading: false,
+                    character: data
+                })
+            })
+    }
+    
+    render() {
+        const text = this.state.loading ? "loading..." : this.state.character.name
+        return (
+            <div>
+                <p>{text}</p>
+            </div>
+        )
+    }
+}
+
+export default App
